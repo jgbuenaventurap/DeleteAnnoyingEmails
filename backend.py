ff85db9deea70bccf09e2ad80fa42ba813b6e2b7
jgbuenaventurap <jbuenaventurap@gmail.com>
2022-10-02 23:11:57 -0500

    prueba1

--- backend.py
+++ backend.py
@@ -6,6 +6,6 @@ API_VERSION="v1"
 SCOPES=["https://mail.google.com/"]
 
 SERVICE=Create_Service(CLIENT_FILE,API_NAME,API_VERSION,SCOPES)
-#SERVICE.users().getProfile(userId="me").execute()
-#SERVICE.users().messages().list(userId="me",q="otraformadeestudiar@palermo.edu").execute()
+SERVICE.users().getProfile(userId="me").execute()
+SERVICE.users().messages().list(userId="me",q="otraformadeestudiar@palermo.edu").execute()
 SERVICE.users().messages().delete(userId="me",id="18376d3f581c8c06").execute()
