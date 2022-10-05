e952dd172ae931c39abe04c19e709e0431556eb2
jgbuenaventurap <jbuenaventurap@gmail.com>
2022-10-05 14:58:51 -0500

    versión 0.1

--- backend.py
+++ backend.py
@@ -6,6 +6,14 @@ API_VERSION="v1"
 SCOPES=["https://mail.google.com/"]
 
 SERVICE=Create_Service(CLIENT_FILE,API_NAME,API_VERSION,SCOPES)
-SERVICE.users().getProfile(userId="me").execute()
-SERVICE.users().messages().list(userId="me",q="otraformadeestudiar@palermo.edu").execute()
-SERVICE.users().messages().delete(userId="me",id="18376d3f581c8c06").execute()
+
+google_dict=SERVICE.users().messages().list(userId="me",q="updates@academia-mail.com").execute()
+google_mails=(google_dict.get('messages'))
+google_id_emails = [item.get("id") for item in google_mails]
+print(len(google_id_emails))
+for ids in google_id_emails:
+
+   SERVICE.users().messages().delete(userId="me",id=ids).execute() 
+   print(f"id correo {ids} ")
+
+
